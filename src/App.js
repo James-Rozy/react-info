37e08a0c84a087ff379a088e0beef542fa9cff50
James Rozsypal <jimmerxl@gmail.com>
2022-12-05 08:18:54 -0500

    Add the Navbar and Main components to render return

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './style.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 
 function App() {
-  return <div className='Container'></div>;
+  return (
+    <div className='Container'>
+      <Navbar />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
